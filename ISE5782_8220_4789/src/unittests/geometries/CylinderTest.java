61c03ba2e59e5fbac4e3bd9d4b23e4f498894603
ziegler <ziegler@g.jct.ac.il>
2022-03-13 14:37:13 +0200

    not finish yet

---
+++ ISE5782_8220_4789/src/unittests/geometries/CylinderTest.java
@@ -0,0 +1,11 @@
+package unittests.geometries;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.;
+
+public class CylinderTest {
+    @Test
+    void testGetNormal() {
+        assertTrue(True);
+    }
+}
