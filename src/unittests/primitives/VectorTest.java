c19c488b07a7b2c4f53632600f31c5d56d81fec6
alonzolti <alonzolti@gmail.com>
2022-03-14 11:23:27 +0200

    vector tests

---
+++ src/unittests/primitives/VectorTest.java
@@ -0,0 +1,46 @@
+package unittests.primitives;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * unit tests for primitives.Vector class
+ */
+public class VectorTest {
+    /**
+     * Test method for{@link primitives.Vector#add(primitives.Vector)}
+     */
+    @Test
+    void testAdd() {
+        
+    }
+
+    @Test
+    void testCrossProduct() {
+
+    }
+
+    @Test
+    void testDotProduct() {
+
+    }
+
+    @Test
+    void testLength() {
+
+    }
+
+    @Test
+    void testLengthSquared() {
+
+    }
+
+    @Test
+    void testNormalize() {
+
+    }
+
+    @Test
+    void testScale() {
+
+    }
+}
