db86f17cd1ce19a876ef8163225aace5971483e0
alonzolti <alonzolti@gmail.com>
2022-03-14 11:23:08 +0200

    add poin and vector test classes

--- src/unittests/primitives/PointTest.java
+++ src/unittests/primitives/PointTest.java
@@ -1,16 +1,23 @@
 package unittests.primitives;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+import primitives.*;
 public class PointTest {
     @Test
     void testAdd() {
+        //=============== Equivalence Partitions Tests =============
+        Point p1 = new Point(1,2,3);
+        Vector p2 = new Vector(-2,-5,7);
 
+        // TC01: Test adding between a point and a vector
+        assertEquals(p1.add(p2), new Point(-1,-3,10));
     }
 
     @Test
     void testDistance() {
-
+        
     }
 
     @Test
