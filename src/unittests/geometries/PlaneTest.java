0202fa9f7299834081545515b1bd6c2fc2a13f9a
ziegler <ziegler@g.jct.ac.il>
2022-03-24 17:29:16 +0200

    1

--- src/unittests/geometries/PlaneTest.java
+++ src/unittests/geometries/PlaneTest.java
@@ -9,14 +9,14 @@ import primitives.*;
 public class PlaneTest {
     @Test
     void testGetNormal() {
-       // ============ Equivalence Partitions Tests ==============
-       Plane p1 = new Plane(new Point(1, 2, 3), new Point(2, 1.2, 4), new Point(3, 4, 5));
-       Plane p2 = new Plane(new Point(1, 2, 3), new Vector(3, 4, 5));
-
+        // ============ Equivalence Partitions Tests ==============
+        Plane p1 = new Plane(new Point(1, 2, 3), new Point(2, 1.2, 4), new Point(3, 4, 5));
+        Plane p2 = new Plane(new Point(1, 2, 3), new Vector(3, 4, 5));
+        Vector v1 = p1.getNormal(new Point(1, 2, 3));
        //TC01: Test that getNormal of Plane made of 3 points works
-       assertEquals(new Vector(-0.7071067811865476, 0.0, 0.7071067811865476),
-               p1.getNormal(new Point(1, 2, 3)),
-               "getNormal() wrong result");
+       assertTrue((new Vector(-0.7071067811865476, 0.0, 0.7071067811865476) == v1 || 
+       new Vector(0.7071067811865476, 0.0, -0.7071067811865476) == v1), 
+       "getNormal() wrong result ");
 
        //TC02: Test that getNormal of Plane made of point and a normal works
        assertEquals(new Vector(3, 4, 5).normalize(), p2.getNormal(new Point(1, 2, 3)),
--- src/unittests/geometries/TriangleTest.java
+++ src/unittests/geometries/TriangleTest.java
@@ -13,11 +13,13 @@ public class TriangleTest {
 
         Triangle t1 = new Triangle(new Point(0,5,0),new Point(-5,-5,0), new Point(-5,5,0));
         Point p1 = new Point(0,0,0);
-        assertEquals(new Vector(0, 0, 1),
-            t1.getNormal(p1),
+        assertTrue((new Vector(0, 0, 1) == t1.getNormal(p1) ||
+            new Vector(0, 0, -1) == t1.getNormal(p1)),
             "getNormal() wrong result");
     }
-
+    // =============== Boundary Values Tests ==================
+    Triangle t1 = new Triangle(new Point(0,5,0),new Point(0,5,0), new Point(-5,5,0));
+    Triangle t2 = new Triangle(new Point(0,0,0), new Point(0,1,0), new Point(0,2,0));
     @Test
     void testFindIntersections()
     {
